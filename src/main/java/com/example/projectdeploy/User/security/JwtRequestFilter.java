09720dc7dbeea359845f4bb34c8c18708531946b
walid <wm75380@gmail.com>
2023-04-19 16:01:20 +0200

    Sign UP Update 4

--- src/main/java/com/example/projectdeploy/User/security/JwtRequestFilter.java
+++ src/main/java/com/example/projectdeploy/User/security/JwtRequestFilter.java
@@ -30,7 +30,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         String jwtToken = null;
         // JWT Token is in the form "Bearer token". Remove Bearer word and get
         // only the Token
-        if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
+        if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer")) {
             jwtToken = requestTokenHeader.substring(7);
             System.out.println(jwtToken);
             System.out.println("*-*-*-*-*-*-*-*-*-*-");
