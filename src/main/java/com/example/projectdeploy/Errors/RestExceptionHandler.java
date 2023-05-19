9fa038996c8b5bd39b8a316285b06b32e3cfe96d
ahmedssameh <59616470+ahmedssameh@users.noreply.github.com>
2023-05-19 17:21:45 +0300

    adding error package

---
+++ src/main/java/com/example/projectdeploy/Errors/RestExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.example.projectdeploy.Errors;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = { Exception.class })
+    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "Internal Server Error";
+        return handleExceptionInternal(ex, bodyOfResponse,
+                new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+}
