c02cb8a55f375753f4d6010d87dbb348b9fbb31b
ahmedssameh <59616470+ahmedssameh@users.noreply.github.com>
2023-06-10 16:11:15 +0300

    Sugar test analysis

---
+++ src/main/java/com/example/projectdeploy/MedicalInformation/SugarBloodTest/SugarAnalysis/SugarAnalysis.java
@@ -0,0 +1,12 @@
+package com.example.projectdeploy.MedicalInformation.SugarBloodTest.SugarAnalysis;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@Setter @Getter
+public class SugarAnalysis {
+    double avgRead;
+    Date date;
+}
