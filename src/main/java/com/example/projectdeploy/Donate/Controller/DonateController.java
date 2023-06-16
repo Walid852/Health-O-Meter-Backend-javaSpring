abd04a0ea1eb7b633ca6cfb418ca759d3a14cac3
walid <wm75380@gmail.com>
2023-06-16 23:32:32 +0300

    Update application.properties for new railway and create donate and update status and Expanding Notification Transmission without testing

--- src/main/java/com/example/projectdeploy/Donate/Controller/DonateController.java
+++ src/main/java/com/example/projectdeploy/Donate/Controller/DonateController.java
@@ -2,9 +2,16 @@ package com.example.projectdeploy.Donate.Controller;
 
 import com.example.projectdeploy.Donate.DTO.DonateRequest;
 import com.example.projectdeploy.Donate.DTO.Resultt;
+import com.example.projectdeploy.Donate.DTO.UpdateStatusRequest;
+import com.example.projectdeploy.Donate.Model.Donate;
+import com.example.projectdeploy.Donate.Model.DonateNotified;
 import com.example.projectdeploy.Donate.Services.CreateDonate;
+import com.example.projectdeploy.Donate.Services.DonateNotifiedUpdateStatus;
+import com.example.projectdeploy.Shared.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @ResponseBody
@@ -12,9 +19,23 @@ import org.springframework.web.bind.annotation.*;
 public class DonateController {
     @Autowired
     CreateDonate createDonate;
+    @Autowired
+    DonateNotifiedUpdateStatus donateNotifiedUpdateStatus;
     @PostMapping(path="/AddDonate")
     public @ResponseBody
-    Resultt AddDonate(@RequestBody DonateRequest donateRequest){
-        return createDonate.CreateDonate(donateRequest);
+    Response<Donate> AddDonate(@RequestBody DonateRequest donateRequest){
+
+        return createDonate.AddDonate(donateRequest);
+    }
+    @PostMapping(path="/ExpandingNotificationTransmission")
+    public @ResponseBody
+    Response<Donate> ExpandingNotificationTransmission(@RequestParam UUID donateId){
+
+        return createDonate.ExpandingNotificationTransmission(donateId);
+    }
+    @PutMapping(path="/UpdateStatus")
+    public @ResponseBody
+    Response<DonateNotified> UpdateStatus(UpdateStatusRequest updateStatusRequest){
+        return donateNotifiedUpdateStatus.UpdateStatus(updateStatusRequest);
     }
 }
--- src/main/java/com/example/projectdeploy/Donate/Services/DonateNotifiedUpdateStatus.java
+++ src/main/java/com/example/projectdeploy/Donate/Services/DonateNotifiedUpdateStatus.java
@@ -3,58 +3,81 @@ package com.example.projectdeploy.Donate.Services;
 import com.example.projectdeploy.Donate.DTO.UpdateStatusRequest;
 import com.example.projectdeploy.Donate.Model.Donate;
 import com.example.projectdeploy.Donate.Model.DonateNotified;
+import com.example.projectdeploy.Donate.Model.LocationHierarchical;
 import com.example.projectdeploy.Donate.Model.Status;
 import com.example.projectdeploy.Donate.Repo.DonateNotifiedRepo;
 import com.example.projectdeploy.Donate.Repo.DonateRepo;
+import com.example.projectdeploy.Notification.Model.ConstantMessage;
+import com.example.projectdeploy.Shared.Response;
+import com.example.projectdeploy.Shared.StaticsText;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.sql.Date;
-
+import java.util.ArrayList;
+import java.util.List;
+@Service
 public class DonateNotifiedUpdateStatus {
     @Autowired
     DonateNotifiedRepo donateNotifiedRepo;
     @Autowired
     DonateRepo donateRepo;
 
-    Boolean AuthorizationForUpdate(UpdateStatusRequest updateStatusRequest){
+    Response<DonateNotified> AuthorizationForUpdate(UpdateStatusRequest updateStatusRequest){
         DonateNotified donateNotified=donateNotifiedRepo.findDonateById(updateStatusRequest.getDonateNotifiedId());
+        List<DonateNotified> result = new ArrayList<>();
+        result.add(donateNotified);
         if(updateStatusRequest.getDonator().toString().equals(donateNotified.getMedicalInformation().toString())&&
                 (updateStatusRequest.getStatus().equals(Status.Agree)||updateStatusRequest.getStatus().equals(Status.Rejected))){
-            if(updateStatusRequest.getStatus().equals(Status.Agree)&&(updateStatusRequest.getDateOfArrival()==null||
-                    updateStatusRequest.getDateOfArrival().before(donateNotified.getDonate().getDonateDate()))){
-                return false;
+            if(     updateStatusRequest.getStatus().equals(Status.Agree)&&
+                    (
+                            updateStatusRequest.getDateOfArrival()==null||
+                            updateStatusRequest.getDateOfArrival().after(donateNotified.getDonate().getDonateDate())
+                    )
+              ){
+                return new Response<>(false, StaticsText.MessageForTest("Can't do that make sure if agree add date before deadline ", ""),result);
             }
-            else return true;
+            else return new Response<>(true, StaticsText.MessageForTest("change status", "successfully"), result);
         }
         else if (updateStatusRequest.getRequstor().toString().equals(donateNotified.getDonate().getRequestorMedicalInformation().toString())&&
                 (updateStatusRequest.getStatus().equals(Status.Approval)||updateStatusRequest.getStatus().equals(Status.Come)
                 ||updateStatusRequest.getStatus().equals(Status.DidNotCome))){
             long now = System.currentTimeMillis();
             Date DateNow = new Date(now);
-            if(donateNotifiedRepo.findDonateNotifiedFoMedicalInformationByStatus(donateNotified.getId(),Status.Approval).size()>2
-            ||((updateStatusRequest.getStatus().equals(Status.Come)
-                    ||updateStatusRequest.getStatus().equals(Status.DidNotCome))&&DateNow.before(donateNotified.getDateOfArrival()))){
-                return false;
+            if(donateNotifiedRepo.findDonateNotifiedFoMedicalInformationByStatus(donateNotified.getId(),Status.Approval).size()>1) {
+                return new Response<>(false, StaticsText.MessageForTest("can't approval more than one", ""), result);
             }
-            return true;
+            if ((updateStatusRequest.getStatus().equals(Status.Come)
+                    || updateStatusRequest.getStatus().equals(Status.DidNotCome)) && DateNow.after(donateNotified.getDateOfArrival())) {
+                return new Response<>(false, StaticsText.MessageForTest("can't choose come or didn't come before deadline", ""), result);
+            }
+            return new Response<>(true, StaticsText.MessageForTest("change status", "successfully"), result);
         }
-        else return false;
+        else return new Response<>(true, StaticsText.MessageForTest("change status", "Unsuccessfully"), result);
     }
-    DonateNotified UpdateStatus(UpdateStatusRequest updateStatusRequest){
-        if(!AuthorizationForUpdate(updateStatusRequest))return  null;
-        DonateNotified donateNotified=donateNotifiedRepo.findDonateById(updateStatusRequest.getDonateNotifiedId());
-        donateNotified.setStatus(updateStatusRequest.getStatus());
-        long now = System.currentTimeMillis();
-        Date DateNow = new Date(now);
-        donateNotified.setLastUpdateDate(DateNow);
-        if(updateStatusRequest.getDateOfArrival()!=null)
-            donateNotified.setDateOfArrival(updateStatusRequest.getDateOfArrival());
-        if(updateStatusRequest.getStatus().equals(Status.Approval)){
-            Donate donate=donateRepo.findDonateById(donateNotified.getDonate().getId());
-            donate.setDonatorMedicalInformation(donateNotified.getMedicalInformation());
-            donateRepo.save(donate);
+    public Response<DonateNotified> UpdateStatus(UpdateStatusRequest updateStatusRequest){
+        try {
+            Response<DonateNotified> donateNotifiedResponse=AuthorizationForUpdate(updateStatusRequest);
+            if(!donateNotifiedResponse.status)return donateNotifiedResponse;
+            DonateNotified donateNotified=donateNotifiedRepo.findDonateById(updateStatusRequest.getDonateNotifiedId());
+            donateNotified.setStatus(updateStatusRequest.getStatus());
+            long now = System.currentTimeMillis();
+            Date DateNow = new Date(now);
+            donateNotified.setLastUpdateDate(DateNow);
+            if(updateStatusRequest.getDateOfArrival()!=null)
+                donateNotified.setDateOfArrival(updateStatusRequest.getDateOfArrival());
+            if(updateStatusRequest.getStatus().equals(Status.Approval)){
+                Donate donate=donateRepo.findDonateById(donateNotified.getDonate().getId());
+                donate.setDonatorMedicalInformation(donateNotified.getMedicalInformation());
+                donate.setCurrent(LocationHierarchical.Terminate);
+                donateRepo.save(donate);
+            }
+            donateNotifiedRepo.save(donateNotified);
+            return donateNotifiedResponse;
         }
-        donateNotifiedRepo.save(donateNotified);
-        return donateNotified;
+        catch (Exception e){
+            return new Response<>(false, StaticsText.MessageForTestError());
+        }
+
     }
 }
