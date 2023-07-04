ee00cc02c9e24f8756f0c2a1c1e7984f9bd1cca9
walid <wm75380@gmail.com>
2023-07-04 08:57:38 +0300

    add notification Request to Notification

--- src/main/java/com/example/projectdeploy/Notification/Model/ResponseForNotification.java
+++ src/main/java/com/example/projectdeploy/Notification/Model/ResponseForNotification.java
@@ -2,6 +2,7 @@ package com.example.projectdeploy.Notification.Model;
 
 import com.example.projectdeploy.Community.Post.Model.Post;
 import com.example.projectdeploy.Donate.Model.Donate;
+import com.example.projectdeploy.Donate.Model.DonateNotified;
 import lombok.Data;
 
 @Data
@@ -10,11 +11,13 @@ public class ResponseForNotification {
     public String message;
     public Post post;
     public Donate donate;
+    public DonateNotified donateNotified;
 
-    public ResponseForNotification(boolean status, String message, Post post, Donate donate) {
+    public ResponseForNotification(boolean status, String message, Post post, Donate donate,DonateNotified donateNotified) {
         this.status = status;
         this.message = message;
         this.post = post;
         this.donate = donate;
+        this.donateNotified=donateNotified;
     }
 }
